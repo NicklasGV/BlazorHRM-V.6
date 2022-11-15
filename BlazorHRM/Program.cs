2498a675f4e9c83092ca05723ebb978f0b428f76
NicklasGV <nick020a@elev.tec.dk>
2022-11-15 19:58:44 +0100

    Login now works with admin Privs

--- BlazorHRM/Program.cs
+++ BlazorHRM/Program.cs
@@ -16,24 +16,26 @@ namespace BlazorHRM
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddSyncfusionBlazor();
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
+            #region Repos
             builder.Services.AddScoped<CityRepository>();
             builder.Services.AddScoped<LoginRepository>();
             builder.Services.AddScoped<EmpRepository>();
             builder.Services.AddScoped<AdminRepository>();
             builder.Services.AddScoped<DepRepository>();
             builder.Services.AddScoped<EmpLoginRepository>();
+            #endregion
 
-
+            #region Services
             builder.Services.AddScoped<CityService>();
             builder.Services.AddScoped<GlobalService>();
             builder.Services.AddScoped<LoginService>();
             builder.Services.AddScoped<EmpService>();
             builder.Services.AddScoped<TaskService>();
             builder.Services.AddScoped<DepService>();
+            #endregion
 
             var app = builder.Build();
 
